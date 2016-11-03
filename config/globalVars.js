3cff434d67e68843ab3a918b14aa75aadbb26664
ifotn <rich@infrontofthenet.com>
2016-11-03 13:42:02 -0400

    end of class files

--- config/globalVars.js
+++ config/globalVars.js
@@ -4,6 +4,7 @@
 
 // global variables for the application
 module.exports = {
-  //db: 'mongodb://localhost/comp2068-thu'
-  db: 'mongodb://gcrfreeman2:iwillnotbelate2016@ds048319.mlab.com:48319/comp2068-thu'
+  db: 'mongodb://localhost/comp2068-thu',
+  secret: 'UseThis to create Salt 123'
+  //db: 'mongodb://uname:pass@ds048319.mlab.com:48319/comp2068-thu'
 };
